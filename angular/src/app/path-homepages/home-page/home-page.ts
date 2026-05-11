bf9a3ed404a6f9179c5960bdef841498e0ea7b61
Manu <adam.manu.raz@gmail.com>
2026-05-11 21:39:19 +0200

    Upgrade - photos and prices

--- angular/src/app/path-homepages/home-page/home-page.ts
+++ angular/src/app/path-homepages/home-page/home-page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NavigationBar} from "../../navigation-bar/navigation-bar";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
-  imports: [
-    NavigationBar
-  ],
+    imports: [
+        NavigationBar,
+        RouterLink
+    ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
